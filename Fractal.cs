8872c0b4737b34f2aafc702d4d4e1a0d27ecd9c0
Jack Wren <proxze@Kristians-MacBook-Pro.local>
2015-10-29 16:03:45 +0000

    Got the code from Java to compile to c#. Still not working but linked all Fractal_Paint and mouse movements so they connect to the form design. At the moment it draw a small red box to the screen, and none of the Mandelbrot draws to the page.

---
+++ Fractal.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Fractal : Form
+    {
+        private const int MAX = 256;      // max iterations
+        private const double SX = -2.025; // start value real
+        private const double SY = -1.125; // start value imaginary
+        private const double EX = 0.6;    // end value real
+        private const double EY = 1.125;  // end value imaginary
+        private static int x1, y1, xs, ys, xe, ye;
+        private static double xstart, ystart, xende, yende, xzoom, yzoom;
+        private static bool action, rectangle, finished;
+        private static float xy;
+        private Graphics g1;
+        private Bitmap myBitmap;
+        //private Cursor c1, c2;
+        ///private HSB HSBcol = new HSB();
+        ///
+
+        public Fractal()
+        {
+            InitializeComponent();
+            init();
+            start();
+            
+        }
+
+        private void Fractal_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+        public struct HSBColor
+        {
+            float h;
+            float s;
+            float b;
+            int a;
+            public HSBColor(float h, float s, float b)
+            {
+                this.a = 0xff;
+                this.h = Math.Min(Math.Max(h, 0), 255);
+                this.s = Math.Min(Math.Max(s, 0), 255);
+                this.b = Math.Min(Math.Max(b, 0), 255);
+            }
+            public HSBColor(int a, float h, float s, float b)
+            {
+                this.a = a;
+                this.h = Math.Min(Math.Max(h, 0), 255);
+                this.s = Math.Min(Math.Max(s, 0), 255);
+                this.b = Math.Min(Math.Max(b, 0), 255);
+            }
+            public float H
+            {
+                get { return h; }
+            }
+            public float S
+            {
+                get { return s; }
+            }
+            public float B
+            {
+                get { return b; }
+            }
+            public int A
+            {
+                get { return a; }
+            }
+            public Color Color
+            {
+                get
+                {
+                    return FromHSB(this);
+                }
+            }
+            public static Color FromHSB(HSBColor hsbColor)
+            {
+                float r = hsbColor.b;
+                float g = hsbColor.b;
+                float b = hsbColor.b;
+                if (hsbColor.s != 0)
+                {
+                    float max = hsbColor.b;
+                    float dif = hsbColor.b * hsbColor.s / 255f;
+                    float min = hsbColor.b - dif;
+                    float h = hsbColor.h * 360f / 255f;
+                    if (h < 60f)
+                    {
+                        r = max;
+                        g = h * dif / 60f + min;
+                        b = min;
+                    }
+                    else if (h < 120f)
+                    {
+                        r = -(h - 120f) * dif / 60f + min;
+                        g = max;
+                        b = min;
+                    }
+                    else if (h < 180f)
+                    {
+                        r = min;
+                        g = max;
+                        b = (h - 120f) * dif / 60f + min;
+                    }
+                    else if (h < 240f)
+                    {
+                        r = min;
+                        g = -(h - 240f) * dif / 60f + min;
+                        b = max;
+                    }
+                    else if (h < 300f)
+                    {
+                        r = (h - 240f) * dif / 60f + min;
+                        g = min;
+                        b = max;
+                    }
+                    else if (h <= 360f)
+                    {
+                        r = max;
+                        g = min;
+                        b = -(h - 360f) * dif / 60 + min;
+                    }
+                    else
+                    {
+                        r = 0;
+                        g = 0;
+                        b = 0;
+                    }
+                }
+                return Color.FromArgb
+                    (
+                        hsbColor.a,
+                        (int)Math.Round(Math.Min(Math.Max(r, 0), 255)),
+                        (int)Math.Round(Math.Min(Math.Max(g, 0), 255)),
+                        (int)Math.Round(Math.Min(Math.Max(b, 0), 255))
+                        );
+            }
+        }
+
+
+        public void init() // all instances will be prepared
+        {
+            //HSBcol = new HSB();
+            finished = false;
+
+            x1 = this.Width;
+            y1 = this.Height;
+            xy = (float)x1 / (float)y1;
+
+            myBitmap  = new Bitmap(x1, y1);
+            g1 = Graphics.FromImage(myBitmap);
+
+            finished = true;
+        }
+
+        public void start()
+        {
+            action = false;
+            rectangle = false;
+            initvalues();
+            xzoom = (xende - xstart) / (double)x1;
+            yzoom = (yende - ystart) / (double)y1;
+            mandelbrot();
+        }
+
+        private void initvalues() // reset start values
+        {
+            xstart = SX;
+            ystart = SY;
+            xende = EX;
+            yende = EY;
+            if ((float)((xende - xstart) / (yende - ystart)) != xy)
+                xstart = xende - (yende - ystart) * (double)xy;
+        }
+
+
+        private void Fractal_Paint(object sender, PaintEventArgs e)
+        {
+            update(g1);
+
+
+        }
+
+        public void update(Graphics g)
+        {
+            g.DrawImage(myBitmap, 0, 0, x1, y1);
+
+            Pen Pen = new Pen(Color.White, 3);
+
+            if (rectangle)
+            {
+                
+                if (xs < xe)
+                {
+                    if (ys < ye) g.DrawRectangle(Pen, xs, ys, (xe - xs), (ye - ys));
+                    else g.DrawRectangle(Pen, xs, ye, (xe - xs), (ys - ye));
+                }
+                else
+                {
+                    if (ys < ye) g.DrawRectangle(Pen, xe, ys, (xs - xe), (ye - ys));
+                    else g.DrawRectangle(Pen, xe, ye, (xs - xe), (ys - ye));
+                }
+            }
+        }
+
+        private void mandelbrot() // calculate all points
+        {
+            
+            Pen tempPen = null;
+            Color col;
+            int x, y;
+            float h, b, alt = 0.0f;
+
+            action = false;
+            Cursor.Current = Cursors.WaitCursor; //setCursor(c1);
+            
+            for (x = 0; x < x1; x += 2)
+                for (y = 0; y < y1; y++)
+                {
+                    h = pointcolour(xstart + xzoom * (double)x, ystart + yzoom * (double)y); // color value
+                    if (h != alt)
+                    {
+                        b = 1.0f - h * h; // brightness
+
+                        /*HSB.fromHSB(h * 255, 0.8f * 255, b * 255);
+
+                        int red = Convert.ToInt32(HSB.rChan); //.getRed();
+                        int green = Convert.ToInt32(HSB.gChan); //.getGreen();
+                        int blue = Convert.ToInt32(HSB.bChan); //.getBlue();
+                        
+                        col = Color.FromArgb(0, red, green, blue);*/
+
+                        col = HSBColor.FromHSB(new HSBColor(h * 255, 0.8f * 255, b * 255));
+
+                        tempPen = new Pen(col, 3);
+                        //djm 
+                        alt = h;
+                    }
+                    g1.DrawLine(tempPen, x, y, x + 1, y);
+                }
+            
+            Cursor.Current = Cursors.Cross; //setCursor(c2);
+            action = true;
+
+            
+        }
+
+        private float pointcolour(double xwert, double ywert) // color value from 0.0 to 1.0 by iterations
+        {
+            double r = 0.0, i = 0.0, m = 0.0;
+            int j = 0;
+
+            while ((j < MAX) && (m < 4.0))
+            {
+                j++;
+                m = r * r - i * i;
+                i = 2.0 * r * i + ywert;
+                r = m + xwert;
+            }
+            return (float)j / (float)MAX;
+        }
+
+
+
+        private void Fractal_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            if (action)
+            {
+                xs = e.X;
+                ys = e.Y;
+            }
+        }
+
+        private void Fractal_MouseUp(object sender, MouseEventArgs e)
+        {
+            int z, w;
+
+            
+            if (action)
+            {
+                xe = e.X;
+                ye = e.Y;
+                if (xs > xe)
+                {
+                    z = xs;
+                    xs = xe;
+                    xe = z;
+                }
+                if (ys > ye)
+                {
+                    z = ys;
+                    ys = ye;
+                    ye = z;
+                }
+                w = (xe - xs);
+                z = (ye - ys);
+                if ((w < 2) && (z < 2)) initvalues();
+                else
+                {
+                    if (((float)w > (float)z * xy)) ye = (int)((float)ys + (float)w / xy);
+                    else xe = (int)((float)xs + (float)z * xy);
+                    xende = xstart + xzoom * (double)xe;
+                    yende = ystart + yzoom * (double)ye;
+                    xstart += xzoom * (double)xs;
+                    ystart += yzoom * (double)ys;
+                }
+                xzoom = (xende - xstart) / (double)x1;
+                yzoom = (yende - ystart) / (double)y1;
+                mandelbrot();
+                rectangle = false;
+                Invalidate();
+            }
+        }
+
+        
+
+        private void Fractal_MouseMove(object sender, MouseEventArgs e)
+        {
+            //e.consume();
+            if (action)
+            {
+                xe = e.X;
+                ye = e.Y;
+                rectangle = true;
+                Invalidate();
+            }
+        }
+
+    }
+}
+
